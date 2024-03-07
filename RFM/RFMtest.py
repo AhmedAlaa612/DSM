da884162335c613d914bc24a20ae3668501d0cdb
Ahmed Aladdin <ahmedalaa555r@gmail.com>
2024-03-07 03:17:01 +0200

    RFM assignment
    simple stupid RFM for hotel customers

---
+++ RFM/RFMtest.py
@@ -0,0 +1,10 @@
+import pandas as pd
+#shes online i want to text her 
+#shes online i want to sleep
+#shes online i like her
+#shes online i want to talk to her
+#shes online i want to see her
+#shes online i want to be with her
+
+
+#gaaaaawwwdddddd
