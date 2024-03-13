f584095ba209d7d0697d0656a3bdf88de95491f5
Ahmed Aladdin <ahmedalaa555r@gmail.com>
2024-03-13 10:08:30 +0200

    Delete DSplayaround directory

--- DSplayaround/irisStreamlit.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd 
-
-
-
-iris = pd.read_csv('Iris.csv')
-st.title('Iris Dataset')
-n = st.slider('Number of rows to view', 1, 150, 5)
-st.dataframe(iris[:n])
